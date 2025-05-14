62faf32e3f6bfad6e139c13e780adb52ce13fa91
E1B11-Wu <u11302211@o365.ttu.edu.tw>
2025-05-14 19:15:15 +0800

    製作選單、功能e

--- P4E1B11.c
+++ P4E1B11.c
@@ -49,4 +49,66 @@ int main() {
         printf("密碼錯誤次數過多，系統結束。\n");
         return 0;
     }
+
+    char choice;
+    while (1) {
+        clearScreen();
+        printf("---------- [Grade System] ----------\n");
+        printf("a. Enter student grades\n");
+        printf("b. Display student grades\n");
+        printf("c. Search for student grades\n");
+        printf("d. Grade ranking\n");
+        printf("e. Exit system\n");
+        printf("------------------------------------\n");
+        printf("請輸入選項：");
+        scanf(" %c", &choice);
+        clearBuffer();
+
+        switch (choice) {
+            case 'a': inputStudents(); break;
+            case 'b': displayStudents(); break;
+            case 'c': searchStudent(); break;
+            case 'd': rankStudents(); break;
+            case 'e': {
+                char confirm;
+                printf("確定離開?(y/n):");
+                scanf(" %c", &confirm);
+                clearBuffer();
+               if (confirm == 'y' || confirm == 'Y')
+				{
+				return 0;
+                break;
+                }
+                
+                else if (confirm == 'N' || confirm == 'n')
+                {
+                	continue;
+				}
+				
+	   			else 
+				{
+				    printf("確定離開?(y/n):");
+                    scanf(" %c", &confirm);
+                    clearBuffer();
+                    if (confirm == 'y' || confirm == 'Y')
+				    {
+			 	    return 0;
+                    break;
+                    }
+                
+                    else if (confirm == 'N' || confirm == 'n')
+                    {
+                	    continue;
+				    }      
+                    else
+                    {
+  printf("無效選項，請重新輸入。\n"); break;
+                    }
+                         
+                 }
+            default: printf("無效選項，請重新輸入。\n"); break;
+        }
+    }
+}
+    
 }
